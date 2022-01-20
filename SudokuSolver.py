edebd81df04ce4102dec58339fe3ec92ef978804
eeyjl11 <92301581+eeyjl11@users.noreply.github.com>
2022-01-20 16:15:52 +0000

    implemented timer function

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -1,15 +1,16 @@
 import numpy as np
+import time
 
 sudokuGrid = np.array([
-[0, 0, 0, 0, 5, 6, 0, 9, 8],
-[5, 0, 0, 8, 0, 4, 0, 0, 0],
-[0, 0, 0, 0, 3, 0, 1, 5, 6],
-[0, 5, 3, 0, 0, 2, 6, 0, 9],
-[0, 8, 0, 4, 0, 0, 3, 7, 0],
-[0, 7, 1, 0, 0, 0, 0, 4, 0],
-[0, 0, 2, 0, 0, 0, 9, 0, 5],
-[0, 6, 0, 5, 0, 0, 0, 0, 0],
-[0, 1, 0, 6, 0, 0, 8, 0, 3]])
+[9,8,3,0,1,0,0,0,0],
+[0,0,0,6,0,0,8,0,0],
+[0,0,0,2,5,0,0,9,0],
+[0,4,1,0,0,7,0,0,2],
+[0,0,0,0,0,0,0,0,0],
+[7,9,0,0,8,0,0,0,0],
+[0,1,2,0,0,0,9,0,0],
+[0,0,0,0,0,0,0,0,7],
+[3,0,8,0,0,0,4,1,0]])
 
 def solveSudokuGrid():
     global sudokuGrid
@@ -41,5 +42,11 @@ def checkValidInput(x, y, num):
                 return 0
             
     return 1
-            
-solveSudokuGrid()
+
+def timer():
+    start = time.time()
+    solveSudokuGrid()
+    end = time.time()
+    print("Time is", end-start, "seconds")
+    
+timer()
